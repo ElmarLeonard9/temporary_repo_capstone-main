9452e18e314aa1467fa9573696465ef1298c77f7
Elmar Leonard <elmarleonard77@gmail.com>
2026-06-23 16:28:43 +0700

    Update 4: box plot and Explanation

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,10 @@
 # This file functions to store all functions that have been created for analysis purposes.
+def rename_cat(category):
+    if category == "Lemari": return "Wardrobe"
+    elif category == "lmri": return "Wardrobe"
+    elif category == "Sofa": return "Sofa & Sofabed"
+    elif category == "sofabed": return "Sofa & Sofabed"
+    elif category == "Dekorasi": return "Decoration"
+    elif category == "Kursi Kerja": return "Office Chair"
+    elif category == "Meja Makan": return "Dining Table"
+    else: return category

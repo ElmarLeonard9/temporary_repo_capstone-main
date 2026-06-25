708031e7777ed726ed0c6689095b96f19d64c045
Elmar Leonard <elmarleonard77@gmail.com>
2026-06-25 14:03:59 +0700

    Update 7: Data transformation (adding new column filled with estimated return cost)

--- src/utils.py
+++ src/utils.py
@@ -8,3 +8,11 @@ def rename_cat(category):
     elif category == "Kursi Kerja": return "Office Chair"
     elif category == "Meja Makan": return "Dining Table"
     else: return category
+
+def fill_ret_cost(category):
+    if category == "Decoration": return 170000
+    elif category == "Office Chair": return 480000
+    elif category == "Dining Table": return 1150000
+    elif category == "Bed": return 1480000
+    elif category == "Sofa & Sofabed": return 1850000
+    else: return 2400000
